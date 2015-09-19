0e67c2dc426c357c8bf92311dafc71463148ed4e
Taylor Fausak <taylor@fausak.me>
2015-09-19 09:23:57 -0500

    Track the current direction

--- library/Snake.hs
+++ library/Snake.hs
@@ -29,6 +29,7 @@ stepRate = 1
 initialWorld :: World
 initialWorld = NewWorld
     { resolution = (640, 480)
+    , direction = North
     }
 
 drawWorld :: World -> G.Picture
@@ -37,6 +38,7 @@ drawWorld _world = G.blank
 handleEvent :: G.Event -> World -> World
 handleEvent event world = case event of
     G.EventResize newResolution -> handleResize newResolution world
+    G.EventKey key state _ _ -> handleKey key state world
     _ -> world
 
 handleStep :: Float -> World -> World
@@ -47,8 +49,24 @@ handleStep _time world = world
 handleResize :: (Int, Int) -> World -> World
 handleResize newResolution world = world { resolution = newResolution }
 
+handleKey :: G.Key -> G.KeyState -> World -> World
+handleKey key state world = case (key, state) of
+    (G.SpecialKey G.KeyUp, G.Down) -> world { direction = North }
+    (G.SpecialKey G.KeyRight, G.Down) -> world { direction = East }
+    (G.SpecialKey G.KeyDown, G.Down) -> world { direction = South }
+    (G.SpecialKey G.KeyLeft, G.Down) -> world { direction = West }
+    _ -> world
+
 --
 
 data World = NewWorld
     { resolution :: (Int, Int)
+    , direction :: Direction
     } deriving (Eq, Ord, Read, Show)
+
+data Direction
+    = North
+    | East
+    | South
+    | West
+    deriving (Bounded, Enum, Eq, Ord, Read, Show)
