64caec68977f5d7d84562ca94c93080302dd57e6
Taylor Fausak <taylor@fausak.me>
2015-09-19 09:04:37 -0500

    Track the resolution

--- library/Snake.hs
+++ library/Snake.hs
@@ -3,19 +3,22 @@ module Snake where
 import qualified Graphics.Gloss.Interface.Pure.Game as G
 
 main :: IO ()
-main = G.play
-    displayMode
-    backgroundColor
-    stepRate
-    initialWorld
-    drawWorld
-    handleEvent
-    handleStep
+main = do
+    let world = initialWorld
+
+    G.play
+        (displayMode world)
+        backgroundColor
+        stepRate
+        world
+        drawWorld
+        handleEvent
+        handleStep
 
 --
 
-displayMode :: G.Display
-displayMode = G.InWindow "Snake" (640, 480) (0, 0)
+displayMode :: World -> G.Display
+displayMode world = G.InWindow "Snake" (resolution world) (0, 0)
 
 backgroundColor :: G.Color
 backgroundColor = G.white
@@ -25,18 +28,27 @@ stepRate = 1
 
 initialWorld :: World
 initialWorld = NewWorld
+    { resolution = (640, 480)
+    }
 
 drawWorld :: World -> G.Picture
 drawWorld _world = G.blank
 
 handleEvent :: G.Event -> World -> World
-handleEvent _event world = world
+handleEvent event world = case event of
+    G.EventResize newResolution -> handleResize newResolution world
+    _ -> world
 
 handleStep :: Float -> World -> World
 handleStep _time world = world
 
 --
 
+handleResize :: (Int, Int) -> World -> World
+handleResize newResolution world = world { resolution = newResolution }
+
+--
+
 data World = NewWorld
-    {
+    { resolution :: (Int, Int)
     } deriving (Eq, Ord, Read, Show)
