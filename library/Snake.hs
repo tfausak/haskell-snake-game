df98e55f866cae75c9d288ab733ce091e63b9a8d
Taylor Fausak <taylor@fausak.me>
2015-09-19 08:51:15 -0500

    Render an empty window with Gloss

--- library/Snake.hs
+++ library/Snake.hs
@@ -1,4 +1,42 @@
 module Snake where
 
+import qualified Graphics.Gloss.Interface.Pure.Game as G
+
 main :: IO ()
-main = putStrLn "Snake!"
+main = G.play
+    displayMode
+    backgroundColor
+    stepRate
+    initialWorld
+    drawWorld
+    handleEvent
+    handleStep
+
+--
+
+displayMode :: G.Display
+displayMode = G.InWindow "Snake" (640, 480) (0, 0)
+
+backgroundColor :: G.Color
+backgroundColor = G.white
+
+stepRate :: Int
+stepRate = 1
+
+initialWorld :: World
+initialWorld = NewWorld
+
+drawWorld :: World -> G.Picture
+drawWorld _world = G.blank
+
+handleEvent :: G.Event -> World -> World
+handleEvent _event world = world
+
+handleStep :: Float -> World -> World
+handleStep _time world = world
+
+--
+
+data World = NewWorld
+    {
+    } deriving (Eq, Ord, Read, Show)
